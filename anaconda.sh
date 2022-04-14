bed6e0564ff26756da17c35bf0fc90993fe60fdb
wzha <wzha38@163.com>
2022-04-14 13:22:14 -0500

    fixed anaconda

--- anaconda.sh
+++ anaconda.sh
@@ -9,19 +9,20 @@ echo "installing anaconda......"
 
 cd ${WORK_DIR}
 #自带python3.6的Anaconda大约是2018年的版本
-curl -O https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
-chmod +x Anaconda3-2018.12-Linux-x86_64.sh
-./Anaconda3-2018.12-Linux-x86_64.sh -b -p ${HOME}/anaconda3
-rm Anaconda3-2018.12-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+chmod +x Anaconda3-2019.10-Linux-x86_64.sh
+./Anaconda3-2019.10-Linux-x86_64.sh -b -p ${HOME}/anaconda3
+rm Anaconda3-2019.10-Linux-x86_64.sh
 eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
 conda init
 
 conda install -y -c anaconda boost cmake
-conda install -y -c conda-forge pymesh2
+#conda install -y -c conda-forge pymesh2
 
 #dos2unix chroma_env.sh
 sudo cp chroma_env.sh /etc/profile.d/
 source /etc/profile
 
 echo "anaconda installed!"
+echo "restart to continue installation"
 echo "==============================="
