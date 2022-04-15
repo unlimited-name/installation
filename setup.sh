b88ce73c84aaf5718ccd38c426f5241216e78c69
wzha <wzha38@163.com>
2022-04-15 00:08:03 -0500

    Solved env problems

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 echo "==========Hello World !============"
 #sudo apt-get update && apt-get install curl git wget
 export WORK_DIR=${HOME}
+sudo cp g4py.4.10.05.p01.patch ${WORK_DIR}
 echo "==============================="
 
 echo "installing CUDA......"
@@ -34,6 +35,10 @@ make -j12 && make install
 cd ${WORK_DIR}
 rm -rf ${WORK_DIR}/root-${VERSION_ROOT}
 
+if [ -e $HOME/root/bin/thisroot.sh ]; then
+    source $HOME/root/bin/thisroot.sh
+fi
+
 echo "ROOT installed!"
 echo "==============================="
 
@@ -48,6 +53,11 @@ tar xf geant4.${VERSION_GEANT4}.tar.gz && rm geant4.${VERSION_GEANT4}.tar.gz
 mkdir ${WORK_DIR}/geant4.${VERSION_GEANT4}/build-g4 && cd ${WORK_DIR}/geant4.${VERSION_GEANT4}/build-g4
 cmake -DCMAKE_BUILD_TYPE=Release -DGEANT4_INSTALL_DATA=ON -DCMAKE_INSTALL_PREFIX=${HOME}/Geant4 -DGEANT4_USE_GDML=ON -DGEANT4_USE_OPENGL_X11=ON -DGEANT4_USE_XM=OFF ${WORK_DIR}/geant4.${VERSION_GEANT4}
 make -j12 && make install
+
+if [ -e $HOME/Geant4/bin/geant4.sh ]; then
+    source $HOME/Geant4/bin/geant4.sh
+    export PYTHONPATH="$HOME/Geant4/lib:$PYTHONPATH"
+fi
 
 export "Geant4 installed!"
 echo "==============================="
